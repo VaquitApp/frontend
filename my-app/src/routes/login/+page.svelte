52484acce3a05593c77022611a3580c3e0a64182
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-12 16:15:10 -0300

    feat: add login API integration

--- my-app/src/routes/login/+page.svelte
+++ my-app/src/routes/login/+page.svelte
@@ -1,16 +1,33 @@
-<form method="POST">
-	<fieldset>
-		<label>
-			Ingrese su email
-			<input type="email" name="email" placeholder="Email" />
-		</label>
-		<label>
-			Ingrese su contraseña
-			<input type="password" name="password" placeholder="Contaseña" />
-			<a href="/recover">Olvidé mi contraseña</a>
-		</label>
-	</fieldset>
+<script>
+	import { title } from '$lib';
 
-	<button type="submit">Ingresar</button>
-	<a href="/register">Registrarme</a>
-</form>
+	/** @type {import('./$types').ActionData} */
+	export let form;
+</script>
+
+<svelte:head>
+	<title>{title} - Login</title>
+</svelte:head>
+
+{#if form?.success}
+	<!-- this message is ephemeral; it exists because the page was rendered in
+		   response to a form submission. it will vanish if the user reloads -->
+	<p>Successfully logged in!</p>
+{:else}
+	<form method="POST">
+		<fieldset>
+			<label>
+				Ingrese su email
+				<input type="email" name="email" placeholder="Email" />
+			</label>
+			<label>
+				Ingrese su contraseña
+				<input type="password" name="password" placeholder="Contaseña" />
+				<a href="/recover">Olvidé mi contraseña</a>
+			</label>
+		</fieldset>
+
+		<button type="submit">Ingresar</button>
+		<a href="/register">Registrarme</a>
+	</form>
+{/if}
