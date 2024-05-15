7ab5c35651290f4b0cd51fcd23d0559f9b130f7f
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-14 22:05:28 -0300

    feat: integrate groups listing with backend

--- my-app/src/routes/groups/movements/[id=integer]/+page.server.ts
+++ my-app/src/routes/groups/movements/[id=integer]/+page.server.ts
@@ -5,26 +5,6 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ cookies, params }) => {
 	const id = Number(params.id);
 	const group: Group = await get(`group/${id}`, getAuthHeader(cookies));
-	// TODO: load real spendings
-	const spendings: Spending[] = [
-		{
-			id: 1,
-			description: 'Entradas de Taylor Swift',
-			amount: 100_000,
-			date: new Date()
-		},
-		{
-			id: 2,
-			description: 'Entradas de Tan Biónica',
-			amount: 20_000,
-			date: new Date()
-		},
-		{
-			id: 3,
-			description: 'Entradas de Boca-River',
-			amount: 180_000,
-			date: new Date()
-		}
-	];
+	const spendings: Spending[] = await get(`group/${id}/spending`, getAuthHeader(cookies));
 	return { group, spendings };
 };
