f7d0a633bad146cadabf0ce7e8867fc588b42738
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-19 18:47:33 -0300

    refactor: extract user credential saving

--- my-app/src/lib/auth.ts
+++ my-app/src/lib/auth.ts
@@ -1,5 +1,10 @@
 import type { Cookies } from '@sveltejs/kit';
 
+export function saveUserCredentials(cookies: Cookies, userId: number) {
+	// TODO: save user info in another cookie
+	cookies.set('jwt', userId.toFixed(0), { httpOnly: true, secure: true, path: '/' });
+}
+
 export function getAuthHeader(cookies: Cookies) {
 	const token = cookies.get('jwt')!;
 	return { 'x-user': token };
