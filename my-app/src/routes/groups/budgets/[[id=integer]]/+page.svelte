1e08465cb9d2d0ee5d5dd174d5cc04e78d5388b8
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2024-05-19 20:58:57 -0300

    feat: add button to edit budget

--- my-app/src/routes/groups/budgets/[[id=integer]]/+page.svelte
+++ my-app/src/routes/groups/budgets/[[id=integer]]/+page.svelte
@@ -4,6 +4,9 @@
 
 	export let data: PageServerData;
 
+	export const PENCIL_SVG =
+		'<svg fill="#FFF" width="25px" height="25px" viewBox="0 0 528.899 528.899"><g><path d="M328.883,89.125l107.59,107.589l-272.34,272.34L56.604,361.465L328.883,89.125z M518.113,63.177l-47.981-47.981 c-18.543-18.543-48.653-18.543-67.259,0l-45.961,45.961l107.59,107.59l53.611-53.611 C532.495,100.753,532.495,77.559,518.113,63.177z M0.3,512.69c-1.958,8.812,5.998,16.708,14.811,14.565l119.891-29.069 L27.473,390.597L0.3,512.69z"/></g></svg>';
+
 	function formatDateString(dateString: string): string {
 		const date = new Date(Date.parse(dateString));
 		const day = date.getDay();
@@ -30,11 +33,18 @@
 </header>
 {#each data.budgets as budget}
 	<article>
-		<header>{budget.description}</header>
+		<header class="row">
+			<b>{budget.description}</b>
+			<p>Categoría: {budget.category_id}</p>
+		</header>
 		<div class="grid">
 			{formatDateString(budget.start_date)} — {formatDateString(budget.end_date)}
-			<p>Categoría: {budget.category_id}</p>
-			<p class="text-right">$ {budget.amount.toFixed(2)}</p>
+			<div class="row">
+				<p class="text-right" style="margin-left: auto; padding-right:5%">
+					$ {budget.amount.toFixed(2)}
+				</p>
+				<a class="secondary" href="/budgets/{budget.id}" role="button">{@html PENCIL_SVG}</a>
+			</div>
 		</div>
 	</article>
 {/each}
